b6ef35ca61e889be19d7a5999472b671503006c9
FredericoStilpen <frederico.stilpen@gmail.com>
2021-05-04 15:54:58 -0300

    first commit

---
+++ SistemaBanco/src/conta/Conta.java
@@ -0,0 +1,21 @@
+package conta;
+
+public class Conta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/* Saque, 
+ * Transferencia, 
+ * Deposito,
+ * AbreConta, 
+ * FechaConta, 
+ * ConsultaSaldo*/
+
+
+
