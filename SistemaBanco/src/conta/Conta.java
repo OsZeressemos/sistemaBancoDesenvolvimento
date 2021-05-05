57d76d3c78ad97e71afecd9086703690e60b5029
FredericoStilpen <frederico.stilpen@gmail.com>
2021-05-05 12:51:17 -0300

    modification

--- SistemaBanco/src/conta/Conta.java
+++ SistemaBanco/src/conta/Conta.java
@@ -1,21 +1,56 @@
 package conta;
 
+/* Saque ok.
+ * Deposito,ok
+ * Transferencia,ok
+ * AbreConta, 
+ * FechaConta, 
+ * ConsultaSaldo*/
+
 public class Conta {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public int agencia;
+	public int numero;
+	public String name;
+	public int cpf; //tipo errado...
+	public int senha;
+	public double saldo;
+	
+	
+	public void saque(double valor) {
+		if (this.saldo>=valor) {
+				this.saldo-=valor; 
+					System.out.println("Saque realizado!");
+		}else {
+			
+			System.out.println("Saldo insuficiente");
+		}
+		
 	}
+	
+		public void deposito(double valor){
+				this.saldo+=valor;	//conferir Void ou return;
+		}
+	
+	
+	public void transfer(double destino, double valor) {
+			if (this.saldo>=valor){
+				 	this.saldo-=valor;
+				 		valor+=destino; //conferir
+		}else {
+			
+			System.out.println("Saldo insuficiente");
+		}
+	
+	}
+	
+	public void abreConta()
+	
+	
 
 }
 
 
-/* Saque, 
- * Transferencia, 
- * Deposito,
- * AbreConta, 
- * FechaConta, 
- * ConsultaSaldo*/
 
 
 
---
+++ SistemaBanco/src/sistema/Principal.java
@@ -0,0 +1,22 @@
+package sistema;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		System.out.println("Welcome to Zeroth National Bank!:");
+		System.out.println("1 - open account");
+		System.out.println("2 - consult balance"); //conferir inglês (kkk)
+		System.out.println("3 - ")
+		
+		case 1 
+		
+		case 2 
+		
+		
+	}
+
+}
