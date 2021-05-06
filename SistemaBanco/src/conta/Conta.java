e05f6bd5fa21905cc1d28a4238eca6a131c7ee08
Volneineves <volneinevesfilho@gmail.com>
2021-05-05 21:44:41 -0300

    update file

--- SistemaBanco/src/conta/Conta.java
+++ SistemaBanco/src/conta/Conta.java
@@ -1,7 +1,7 @@
 package conta;
 
 import sistemExceptions.DepositoIndevido;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 /* Saque ok.
  * Deposito,ok
@@ -18,21 +18,20 @@ public class Conta {
 	public int senha;
 	public double saldo;
 	public int tipo;
-	
-	
-	public Conta(){
-		
+
+	public Conta() {
+
 	}
-	
-	public Conta(int cpf, int numero, int agencia, double saldo, int tipo){
+
+	public Conta(int cpf, int numero, int agencia, double saldo, int tipo) {
 		this.cpf = cpf;
-		this.numero=numero;
-		this.agencia=agencia;
-		this.saldo=saldo;
-		this.tipo=tipo;
-		
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.tipo = tipo;
+
 	}
-	
+
 	public boolean saque(double valor) {
 		if (this.saldo < valor) {
 			System.out.println("Saldo insuficiente!");
@@ -43,7 +42,7 @@ public class Conta {
 			return true;
 
 		}
-	
+
 	}
 
 	public void deposito(double valor) {
@@ -56,7 +55,7 @@ public class Conta {
 
 	public boolean transfer(Conta destino, double valor) {
 		if (this.saldo >= valor) {
-			destino.saldo=destino.saldo+valor;
+			destino.saldo = destino.saldo + valor;
 			this.saldo -= valor;
 			return true;
 		} else {
@@ -65,64 +64,61 @@ public class Conta {
 		}
 
 	}
-	
-	
+
 	public void consultaSaldo() {
-        System.out.printf("Seu saldo �: "+"R$ %.2f%n"+this.saldo);
-    }
-	
+		System.out.printf("Seu saldo �: " + "R$ %.2f%n" + this.saldo);
+	}
+
 	// Getters and setters
-	
+
 	public double getSaldo() {
-        return this.saldo;
-    }
+		return this.saldo;
+	}
 
-    public int getCpfTitular() {
-        return cpf;
-    }
+	public int getCpfTitular() {
+		return cpf;
+	}
 
-    public void setCpfTitular(int cpf) {
-        this.cpf = cpf; // verificar sobre atualiza��o de cadastro.
-    }
+	public void setCpfTitular(int cpf) {
+		this.cpf = cpf; // verificar sobre atualiza��o de cadastro.
+	}
 
-    public int getAgencia() {
-        return agencia;
-    }
+	public int getAgencia() {
+		return agencia;
+	}
 
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
 
-    public int getNumero() {
-        return numero;
-    }
+	public int getNumero() {
+		return numero;
+	}
 
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 
-    public String getTitular() {
-        return titular;
-    }
+	public String getTitular() {
+		return titular;
+	}
 
-    public void setTitular(String titular) {
-        this.titular = titular;
-    }
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
 
-    public int getTipo() {
-        return tipo;
-    }
+	public int getTipo() {
+		return tipo;
+	}
 
-    public void setTipo(int tipo) {
-        this.tipo = tipo;
-    }
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
 
-    @Override
-    public String toString() {
-        return "Conta [titular=" + titular + ", numero=" + numero + ", agencia=" + agencia + ", tipo=" + tipo
-                + ", cpfTitular=" + cpf + ", saldo=" + saldo + "]";
-    }
-	
-
+	@Override
+	public String toString() {
+		return "Conta [titular=" + titular + ", numero=" + numero + ", agencia=" + agencia + ", tipo=" + tipo
+				+ ", cpfTitular=" + cpf + ", saldo=" + saldo + "]";
+	}
 
 }
